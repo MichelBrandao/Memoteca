c80e1c4df679a04996f2795486e0d9d0a1f00881
michel brandao <michelbrandaog@gmail.com>
2026-09-21 21:23:56 -0300

    feat/config os arquivos js no index.html

--- js/ui.js
+++ js/ui.js
@@ -5,7 +5,7 @@ const ui = {
     const listaPensamentos = document.getElementById("lista-pensamentos");
     try {
       const pensamentos = await api.buscaPensamentos();
-      pensamentos.array.forEach((pensamento) => {
+      pensamentos.forEach((pensamento) => {
         listaPensamentos.innerHTML += `
           <li class="li-pensamento" data-id="${pensamento.id}">
           <img src="assets/imagens/aspas-azuis.png" alt="Aspas azuis" class="icone-aspas">
