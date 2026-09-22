ebf61348d7a33fc31c58e87dbdd9de537df1b4e4
michel brandao <michelbrandaog@gmail.com>
2026-09-21 21:09:36 -0300

    feat/config seções de main.js e ui.js na pasta js

--- js/api.js
+++ js/api.js
@@ -3,9 +3,10 @@ const api = {
   async buscaPensamentos() {
     try {
       const response = await fetch("http://localhost:3000/pensamentos");
-      return response.json();
+      return await response.json();
     } catch {
       alert("Erro ao buscar pensamentos");
+      throw error;
     }
   },
 };
---
+++ js/main.js
@@ -0,0 +1,6 @@
+import ui from "./ui.js";
+
+//DOMContentLoaded: quando o arquivo terminar de carregar, executa a ação
+document.addEventListener("DOMContentLoaded", () => {
+  ui.renderizarPensamentos();
+});
